997dc68368cb71e2d78afa15dbbaea75d95f1b3f
Chris Meyer <cmeyer1969@gmail.com>
2019-01-08 14:59:23 -0800

    Initial feedstock commit with conda-smithy 3.2.3.dev37.

---
+++ recipe/bld.bat
@@ -0,0 +1,2 @@
+%PYTHON% -m pip install --no-deps --ignore-installed https://github.com/nion-software/nionswift-tool/releases/download/%PKG_VERSION%/nionswift_tool-%PKG_VERSION%-cp36.cp37-none-win_amd64.whl
+if errorlevel 1 exit 1
---
+++ recipe/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install using pip from the whl files on PyPI
+
+if [ `uname` == Darwin ]; then
+    WHL_FILE=https://github.com/nion-software/nionswift-tool/releases/download/${PKG_VERSION}/nionswift_tool-${PKG_VERSION}-cp36.cp37-abi3-macosx_10_11_intel.whl
+fi
+
+if [ `uname` == Linux ]; then
+    WHL_FILE=https://github.com/nion-software/nionswift-tool/releases/download/${PKG_VERSION}/nionswift_tool-${PKG_VERSION}-cp36.cp37-abi3-manylinux1_x86_64.whl
+fi
+
+pip install --no-deps --ignore-installed $WHL_FILE
